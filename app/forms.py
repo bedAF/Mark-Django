1dc17c2d625535ea2ddb1fc222997206d02fbdfd
RogolinoMark <130138389+RogolinoMark@users.noreply.github.com>
2023-05-17 10:12:32 -0700

    Mark 051703

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class EmailForm(forms.Form):
-    recipients = forms.CharField(label='Recipient', widget=forms.EmailInput)
+    recipients = forms.CharField(label='Recipient', widget=forms.Textarea)
     topic = forms.CharField(label='Topic')
     imagine = forms.CharField(label='Describe the image you want to see')
     # attachment = forms.FileField(label='Attachment', required=False)
