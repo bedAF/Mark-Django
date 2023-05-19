87545b5820be3b20c1bd05994821091051933fa3
bedAF <bed@afterflea.com>
2023-05-19 18:22:37 +0100

    Patch1

--- pathfinder/settings.py
+++ pathfinder/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static/app')
+    os.path.join(BASE_DIR, 'app/static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
